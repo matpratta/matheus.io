fb463d86c529e042e484d5c6fb7893462c71ff33
Matheus Pratta <matpratta@gmail.com>
2019-12-01 20:21:22 -0300

    Google Analytics support

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import GoogleAnalytics from 'vue-analytics'
+Vue.use(GoogleAnalytics, {
+  // Replace this with your GA tracking code
+  id: 'UA-11212699-10'
+})
+
 Vue.config.productionTip = false
 
 new Vue({
